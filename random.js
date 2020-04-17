479395144f3708232981548bf33a0ead06074dce
Alan Velazquez <alan2719.ahcv@gmail.com>
2020-04-17 09:55:13 -0500

    first commit

---
+++ random.js
@@ -0,0 +1,21 @@
+//Create a random password with Uppercase letters, lowercase letters, numbers 
+//and special characters.
+
+//The user can choice wether of the previous options use.
+
+//The user can choice the number of characters of the password from 8 to 128. 
+
+
+//Arrays for the letters and special characters. Generate a random number to
+//pick a letter or special character from the array in a for loop until the 
+//characters reached the numbers of the user want.
+
+// 1. - Ask the user how many characters want for the password
+// 2. - Ask the user if he/she wants Uppercase letters in the password
+// 3. - Ask the user if he/she wants lowercase letters in the password
+// 2. - Ask the user if he/she wants special characters in the password
+// 2. - Ask the user if he/she wants numbers in the password
+
+//Depending of the answers I can send the parameters to the function that the
+// user needs. 
+
---
+++ script.js
@@ -0,0 +1,144 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");    //Returns the first element that matches a specified CSS selector(s) in the document.
+
+function UppercaseLetter(length,repo){
+  var Uletters = ['A','B','C','D','E','F','G','H','I','J',
+  'K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+  for (var i = 0; i < length; i++) {
+    var random = Math.floor(Math.random()*Uletters.length);
+    console.log(random);
+    repo.push(Uletters[random]);
+  }
+  console.log(repo);
+  return (repo)
+};
+
+function LowercaseLetter(length,repo) {
+  var Lletters = ['a','b','c','d','e','f','g','h','i','j',
+  'k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+  l = Math.floor(length /4);
+  for (var i = 0; i < l; i++) {
+    var random = Math.floor(Math.random()*Lletters.length);
+    var indexRandom = Math.floor(Math.random()*repo.length);
+    console.log(random);
+    repo[indexRandom] = (Lletters[random]);
+  }
+  console.log(repo);
+  return (l,repo)
+};
+
+function RandomCharacters (length,repo) {
+  var SpecialCharacters = ['!','#','$','%','&','/','(',')','=','?',
+  '¡','¿','+','-','_','*','~','{','}','[',']',':',';',',','|','"'];
+  l = Math.floor(length /4);
+  for (var i = 0; i < l; i++) {
+    var random = Math.floor(Math.random()*SpecialCharacters.length);
+    var indexRandom = Math.floor(Math.random()*repo.length);
+    console.log(random);
+    repo[indexRandom] = (SpecialCharacters[random]);
+  }
+  console.log(repo);
+  return (l,repo);
+};
+
+function RandomNumbers(length,repo) {
+  l = Math.floor(length /4);
+  for (var i = 0; i < l; i++) {
+    var random = Math.floor(Math.random()*11);
+    var indexRandom = Math.floor(Math.random()*repo.length);
+    console.log(random);
+    repo[indexRandom] = random;
+  }
+  console.log(repo);
+  console.log(repo.join(""));
+  return repo.join("");
+};
+
+var passwordText = [
+  {q: 'Do you want to have Uppercase letters in your password?',
+    a:'y',
+  },
+  {q: 'Do you want to have lowercase letters in your password?',
+    a:'y'
+  },
+  {q: 'Do you want to have special characters in your password?',
+    a:'y'
+  },
+  {q: 'Do you want to have numbers in your password?',
+    a:'y'
+  }
+];
+
+function generatePassword(){
+  var passwordLength = prompt('Enter the number of characters between 8 and 128: ');
+  var answer = "";
+  var boolToString = "";
+  var AnswerList = [];
+  var repo = [];
+
+  if (passwordLength) {
+    for (var i = 0; i < passwordText.length; i++) {
+      answer = confirm(passwordText[i].q);
+
+      if (answer) {
+        boolToString = 'y';
+        AnswerList.push(boolToString);
+      } else {
+        boolToString = 'n';
+        AnswerList.push(boolToString);
+      }
+    }
+    console.log(AnswerList);
+
+    for (var j = 0; j < AnswerList.length; j++) {
+      if (j === 0){
+
+        if (AnswerList[j] === passwordText[j].a) {
+          UppercaseLetter(passwordLength,repo);
+          } else {
+            passwordLength = passwordLength;
+          } 
+        }
+
+      if (j === 1) {
+        if (AnswerList[j] === passwordText[j].a) {
+          LowercaseLetter(passwordLength,repo);
+        } else {
+          passwordLength = passwordLength;
+        } 
+      }
+          
+      if (j === 2) {
+        if (AnswerList[j] === passwordText[j].a) {
+          RandomCharacters(passwordLength,repo);
+        } else {
+          passwordLength = passwordLength;
+        }
+      }
+
+      if (j === 3) {
+        if (AnswerList[j] === passwordText[j].a) {
+          var password = RandomNumbers(passwordLength,repo);
+          return password;
+        } else{
+            var password = RandomCharacters(passwordLength,repo);
+            return password;
+          }
+        }
+    }  
+  } else {
+    alert("No valid length");}
+};
+
+// Write password to the #password input
+function writePassword() {                                //writePassword function
+  var password = generatePassword();                      //Password its equal to the function that will generate the password.
+  var passwordText = document.querySelector("#password"); //Returns the first element that matches a specified CSS selector(s) in the document.
+
+  passwordText.value = password;                          //Value is a property of passwordText
+
+}
+
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);   //When click the button, call writePassword function
